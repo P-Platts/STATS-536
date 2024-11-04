3de573c014cc4670b95f9c8573eb65775bfc1a03
Patric Platts <157041242+P-Platts@users.noreply.github.com>
2024-11-04 12:10:39 -0700

    model evaluation

--- HW 3/Homework3Models.R
+++ HW 3/Homework3Models.R
@@ -5,6 +5,7 @@ library(mgcv)
 library(MASS)
 library(Metrics)
 library(tidyverse)
+library(gridExtra)
 
 # Exploratory Data Analysis
 school <- read.table("STATS-536/HW 3/SchoolResults.txt", header = T, sep = " ")
@@ -117,14 +118,100 @@ poly_test_predict <- predict(poly_model, newdata = X_test, type = "response")
 poly_test_RMSE <- rmse(y_test, poly_test_predict)
 poly_test_RMSE
 
+###
+### Testing LOOCV to get the RMSE for a ploynomial fit model
+###
+
+poly.max.df = 7
+poly.test.errors = matrix(NA, ncol=poly.max.df, nrow=nrow(school))
+
+for(i in 1:nrow(school)){
+  train2 = school[-i,]
+  test2 = school[i,]
+  
+  for(j in 1:poly.max.df){
+    model = lm(Score ~ Lunch + Computer + Expenditure +
+                 poly(Income) + poly(English, degree = j) + poly(STratio, degree = j),,data=train)
+    yhat = predict(model,newdata = test)
+    poly.test.errors[i,j] = test$Score - yhat
+  }
+
+}
+
+sqrt(colMeans(poly.test.errors^2))
 
 ### 
 ### Visualization
 ###
 
-par(mfrow=c(2,2))  # Adjust plot layout to display 2x2
+par(mfrow=c(1,3))  # Adjust plot layout to display 2x2
 plot(gam_model, se=TRUE, shade=TRUE, col="blue")
 
-# Optional: You can extract partial effects and customize with ggplot2
-# Extracting partial effects from the model
-vis_gam(gam_model, view = c("Lunch", "Income"), plot.type = "contour", color="terrain")
+
+# Set up the plotting area with increased margins and space for labels
+par(mfrow=c(1,3), mar=c(5,4,2,2), oma=c(0,0,2,0))  # Adjust margins and layout
+
+# Plot the GAM smooth for Lunch
+plot(gam_model, select = 1, se = TRUE, shade = TRUE, 
+     col = "green4", 
+     shade.col = "lightgreen",
+     xlab = "Lunch", 
+     ylab = "Smooth Estimate",
+     main = "Effect of Lunch on Score", 
+     ylim = c(-100, 100),
+     lwd = 2)  # Adjust y-limits for better visualization
+grid()
+
+# Plot the GAM smooth for Income
+plot(gam_model, select = 2, se = TRUE, shade = TRUE, 
+     col = "gold4", 
+     shade.col = "lightgoldenrod",
+     xlab = "Income", 
+     ylab = "Smooth Estimate",
+     main = "Effect of Income on Score", 
+     ylim = c(-100, 100),
+     lwd = 2)  # Adjust y-limits for better visualization
+grid()
+
+# Plot the GAM smooth for English
+plot(gam_model, select = 3, se = TRUE, shade = TRUE, 
+     col = "firebrick", 
+     shade.col = "lightcoral",
+     xlab = "English", 
+     ylab = "Smooth Estimate",
+     main = "Effect of English on Score", 
+     ylim = c(-100, 100),
+     lwd = 2)  # Adjust y-limits for better visualization
+grid()
+
+# Add a super title to the overall plot
+mtext("GAM Smooth Plots", outer = TRUE, cex = 1.5)
+
+
+par(mfrow=c(1,1))
+
+# Create the Income vs. Score plot
+plot_income <- ggplot(school, aes(x = Income, y = Score)) +
+  geom_point(color = "gold4") +  # Set points color to gold4
+  geom_smooth(method = "gam", formula = y ~ s(x), color = "gold4", se = FALSE) +  # Smooth line in gold4
+  ggtitle("Income vs Score") +
+  xlab("Income") +
+  ylab("Score") +
+  theme(panel.grid = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+
+# Create the English vs. Score plot
+plot_english <- ggplot(school, aes(x = English, y = Score)) +
+  geom_point(color = "firebrick") +  # Set points color to firebrick
+  geom_smooth(method = "gam", formula = y ~ s(x), color = "firebrick", se = FALSE) +  # Smooth line in firebrick
+  ggtitle("English vs Score") +
+  xlab("English") +
+  ylab("Score") +
+  theme(panel.grid = element_blank(),
+        plot.title = element_text(hjust = 0.5))
+
+# Combine the plots side by side
+combined_plot <- plot_income + plot_english + plot_layout(ncol = 2)
+
+# Save the combined plot as a PDF
+ggsave("income_lunch_plots.pdf", combined_plot, width = 10, height = 5)
